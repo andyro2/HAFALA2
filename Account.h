27641fb898d0cdef986585d3da98ce978e946da4
zacharie12 <45387202+zacharie12@users.noreply.github.com>
2018-12-21 12:29:10 +0200

    Add files via upload

---
+++ Account.h
@@ -0,0 +1,35 @@
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <iostream>
+#include <pthread.h>
+#include <semaphore.h>
+#include <string>
+
+
+using std::string;
+
+#define SIZE_PASSWORD 4
+
+class Account
+{
+public:
+	Account();
+	~Account();
+	Account(int num, char* password,int balance);
+	bool check_if_vip();
+	void upgrade_to_vip(char* password);
+	void update_balance(int money, char* password, int* balance);
+
+private:
+	int num_;
+	char* password_;
+	int balance_;
+	bool vip_;
+
+};
+
+
+#endif
